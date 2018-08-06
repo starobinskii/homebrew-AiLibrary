c2e6bd8bd3f233fa2be64799242ba5d5a09abbc3
Egor Starobinskii <st.eb@ailurus.ru>
2018-08-06 20:11:51 +0300

    Adder formula

---
+++ AiLibrary.rb
@@ -0,0 +1,28 @@
+class AiLibrary < Formula
+  desc "C++ Library from Ailurus Studio"
+  homepage "https://github.com/starobinskii/AiLibrary"
+  url "https://github.com/starobinskii/AiLibrary/archive/v1.1.0.tar.gz"
+  sha256 "037b0306eabceca7566959064aa1cda96df6078ae8dae191bbefbf6932f8fb9e"
+  
+  head "https://github.com/starobinskii/AiLibrary.git", :branch => "master"
+
+  def install
+    include.install ""
+    ohai "to use the library, please set your include path accordingly:"
+    ohai "CPPFLAGS: -I#{include}"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <ai.hh>
+      int main(){
+        ai::printLine(ai::string("This is AiLibrary v")
+          + ai::getVersion()+ ai::string(", welcome!")
+        );
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cc", "-I#{include}", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
