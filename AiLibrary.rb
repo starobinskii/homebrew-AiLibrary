a2578156ac03c3ddf8f0b7e12be72fcef5df2db6
Egor Starobinskii <st.eb@ailurus.ru>
2019-02-10 15:24:10 +0300

    Updated to v1.3.0

--- AiLibrary.rb
+++ AiLibrary.rb
@@ -1,8 +1,8 @@
 class Ailibrary < Formula
   desc "C++ Library from Ailurus Studio"
   homepage "https://github.com/starobinskii/AiLibrary"
-  url "https://github.com/starobinskii/AiLibrary/archive/v1.2.0.tar.gz"
-  sha256 "f5e96c4749571f3cc82159298899695a8fcb801c8761b2dad664fe6794536bc8"
+  url "https://github.com/starobinskii/AiLibrary/archive/v1.3.0.tar.gz"
+  sha256 "f8da497ab2a81667b86d480d451d767bdf38bacea1ff606e404e314b503fdf8f"
   
   head "https://github.com/starobinskii/AiLibrary.git", :branch => "master"
 
@@ -17,7 +17,7 @@ class Ailibrary < Formula
       #include <ai.hh>
       int main(){
         ai::printLine(ai::string("This is AiLibrary v")
-          + ai::getVersion()+ ai::string(", welcome!")
+          + ai::getVersion() + ai::string(", welcome!")
         );
         return 0;
       }
